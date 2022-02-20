96a37f0018506d1bb3510a7f881088a7d2f0268f
The Level Three Chin <henchelsantillan@gmail.com>
2022-02-19 22:08:42 -0500

    Minor ESC + motor control updates

--- flying_garbage/flying_garbage.ino
+++ flying_garbage/flying_garbage.ino
@@ -57,14 +57,31 @@ float phiFOld = 0, phiFNew, thetaFOld = 0, thetaFNew;
 float tElapsed, tCurrent = 0, tPrevious;
 
 
-//-------- I2C --------//
-byte recvFromI2C;
-unsigned long startTimeI2C;
-
-
 //-------- ESC --------//
-Servo ESC_TOP;    // ESC for the top rotor / blade
-Servo ESC_TAIL;   // ESC for the tail rotor / blade
+
+#define ESC_HIGH_DEF 200
+#define ESC_LOW_DEF 20
+
+Servo ESCTop;    // ESC for the top rotor / blade
+Servo ESCTail;   // ESC for the tail rotor / blade
+
+typedef struct MotorType {
+  Servo esc;
+  int pin;
+};
+
+typedef struct ESCSettingsType {
+  int low;
+  int high;
+};
+
+int step = 10;
+int currentSpeed;
+
+MotorType motorTail;
+MotorType motorTop;
+
+ESCSettingsType ESCSettings;
 
 
 //-------- HELPER FUNCTIONS --------//
@@ -89,6 +106,7 @@ void computeErrorIMU() {
     Wire.write(0x3B);
     Wire.endTransmission(false);
     Wire.requestFrom(MPU_I2C_ADDR, 6, true);  // next 6 registers (from 0x3B --> 0x42)
+    while (Wire.available() < 6);
 
     accX = Wire.read() << 8 | Wire.read();
     accY = Wire.read() << 8 | Wire.read();
@@ -111,6 +129,7 @@ void computeErrorIMU() {
     Wire.write(0x3B);
     Wire.endTransmission(false);
     Wire.requestFrom(MPU_I2C_ADDR, 6, true);  // next 6 registers (from 0x3B --> 0x42)
+    while (Wire.available() < 6);
     
     accX = Wire.read() << 8 | Wire.read();
     accY = Wire.read() << 8 | Wire.read();
@@ -130,6 +149,7 @@ void computeErrorIMU() {
     Wire.write(0x43);
     Wire.endTransmission(false);
     Wire.requestFrom(MPU_I2C_ADDR. 6, true);  // from 0x43 --> 0x48
+    while (Wire.available() < 6);
 
     gyroX = Wire.read() << 8 | Wire.read();
     gyroY = Wire.read() << 8 | Wire.read();
@@ -182,8 +202,18 @@ void setup() {
   btModule.begin(BAUD_RATE);
 
   // Attach the ESCs to their respective pins and specify the minimum and maximum pulse widths
-  ESC_TOP.attach(ESC_TOP_PIN, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
-  ESC_TAIL.attach(ESC_TAIL_PIN, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  ESCTop.attach(ESC_TOP_PIN, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  ESCTail.attach(ESC_TAIL_PIN, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+
+  // Tie ESCs and pins to convenience structs
+  motorTail.esc = ESCTop;
+  motorTail.pin = ESC_TOP_PIN;
+  motorTop.esc = ESCTail;
+  motorTop.pin = ESC_TAIL_PIN;
+
+  // Init the ESCSettingsType
+  ESCSettings.low = ESC_LOW_DEFAULT;
+  ESCSettings.high = ESC_HIGH_DEFAULT;
 
   // Init MPU6050 and validate
   if (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G) {
@@ -196,14 +226,14 @@ void setup() {
 }
 
 
-
 void loop() {
   // Read accelerometer data IN
   Wire.beginTransmission(MPU_I2C_ADDR);
   Wire.write(0x3B); // 0x3B register is ACCEL_XOUT_H, which contains the 8 most significant bits for accelerometer readings)
   Wire.endTransmission(false);  // Keep connection active, since Arduino will send restart.
   Wire.requestFrom(MPU_I2C_ADDR, 14, true); // Request a total of 14 registers
-
+  while (Wire.available() < 6);
+  
   // Conv 2 8-bit (1 byte) reads into 1 16-bit output (left shift with 8 or 2^3, and bitwise OR with next read)
   // Need this since accelerometer values are 16 bit (2 bytes), and Wire.read() only does 1 byte at a time
   accX = Wire.read() << 8 | Wire.read();
@@ -213,7 +243,7 @@ void loop() {
   accAngleX = roll(accX, accY, accZ) - errAX;
   accAngleY = pitch(accX, accY, accZ) - errAY;
 
-  // Low filter for accelerometer data
+  // Low pass filter for accelerometer data
   phiFNew = 0.9 * phiFOld + 0.1 * accAngleX;
   thetaFNew = 0.9 * thetaFOld + 0.1 * accAngleY;
 
@@ -227,12 +257,13 @@ void loop() {
   Wire.write(0x43);
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_I2C_ADDR, 6, true);
+  while (Wire.available() < 6);
   
   gyroX = (Wire.read() << 8 | Wire.read()) - errGX;
   gyroY = (Wire.read() << 8 | Wire.read()) - errGY;
   gyroZ = (Wire.read() << 8 | Wire.read()) - errGZ;
 
-  // Complementary filter: combine raw gyro angle data with low filter for accelerometer data
+  // Complementary filter (high-pass filter): combine raw gyro angle data with low filter for accelerometer data
   gyroAngleX = 0.96 * (gyroAngleX + gyroX * elapsedTime) + 0.04 * phiFNew;  // deg/s * s = deg
   gyroAngleY = 0.96 * (gyroAngleY + gyroY * elapsedTime) + 0.04 * phiFNew;
 
@@ -240,13 +271,21 @@ void loop() {
   pitch = gyroAngleY;
   yaw = yaw + gyroZ * elapsedTime;
 
+  Serial.print("Roll:");
+  Serial.println(roll);
+
+  Serial.print("Pitch:");
+  Serial.println(pitch);
+
+  Serial.print("Yaw:");
+  Serial.println(yaw);
+
   // Update the low filter values for accelerometer values
   phiFOld = phiFNew;
-  thetaFOld = thetaFNew;
+  thetaFold = thetaFNew;
 
   delay(10);
   
-
   // Reads bluetooth module if bytes available for reading > 0
   if (btModule.available() > 0) {
     flag = btModule.read();
